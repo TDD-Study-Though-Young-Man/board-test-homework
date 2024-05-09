9be03aa49a34e2a0f9dab358f1990209984b040d
min <dr94406@naver.com>
2024-05-09 17:43:19 +0900

    ⭐feat: ErrorCode Enum 추가

--- src/main/java/com/member/homework/exception/ErrorCode.java
+++ src/main/java/com/member/homework/exception/ErrorCode.java
@@ -10,6 +10,11 @@ import static org.springframework.http.HttpStatus.*;
 public enum ErrorCode {
 
     /* 400 BAD_REQUEST : 잘못된 요청 */
+    NOT_CREATED_INSTANCE(BAD_REQUEST, "인스턴스를 생성할 수 없습니다."),
+    NOT_BE_EMPTY_MEMBER_ID(BAD_REQUEST, "아이디는 비어있을 수 없습니다."),
+    NOT_BE_EMPTY_MEMBER_PASSWORD(BAD_REQUEST, "비밀번호는 비어있을 수 없습니다."),
+    NOT_BE_EMPTY_MEMBER_NAME(BAD_REQUEST, "이름은 비어있을 수 없습니다."),
+    ID_CAN_NOT_BE_LESS_THAN_ZERO(BAD_REQUEST, "아이디는 0 미만일 수 없습니다"),
 
     /* 403 FORBIDDEN : 접근 권한 제한 */
     /* Valid : 유효한 */
