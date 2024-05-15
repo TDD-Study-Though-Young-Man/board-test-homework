4eb094080f1f67150b88d989bbf7080b66801aa0
pgrrr <pgrrr119@gmail.com>
2024-05-15 18:35:58 +0900

    ♻️ refactor: Comment repository 테스트 코드 수정
    - 스터디 피드백 반영
    - 가독성을 위해 하나의 테스트 메서드 내 테스트 내용을 모두 포함하도록 수정
    - @BeforeEach 제거

--- src/test/java/com/member/homework/repository/comment/CommentRepositoryTest.java
+++ src/test/java/com/member/homework/repository/comment/CommentRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.member.homework.repository.comment;
 
 import com.member.homework.domain.Comment;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,34 +8,28 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-public class CommentRepositoryTest {
+class CommentRepositoryTest {
 
     @Autowired
     private CommentRepository commentRepository;
 
-    private Comment comment;
-    String commentContent = "테스트 댓글 내용";
-    String commentAuthor = "테스트 댓글 작성자";
-
-    @BeforeEach
-    void setUp() {
-        comment = Comment.builder()
-                .author(commentAuthor)
-                .content(commentContent)
+    @Test
+    void 새로운_댓글을_추가할_수_있어야_한다() {
+        //given
+        Comment comment = Comment.builder()
+                .author("테스트 댓글 작성자")
+                .content("테스트 댓글 내용")
                 .deleteYn(false)
                 .activeYn(true)
                 .build();
-    }
 
-    @Test
-    void 새로운_댓글을_추가할_수_있어야_한다() {
          //when
         Comment savedComment = commentRepository.save(comment);
 
         //then
         Comment foundComment = commentRepository.findById(savedComment.getCommentId()).orElseThrow();
-        assertThat(foundComment.getAuthor()).isEqualTo(commentAuthor);
-        assertThat(foundComment.getContent()).isEqualTo(commentContent);
+        assertThat(foundComment.getAuthor()).isEqualTo("테스트 댓글 작성자");
+        assertThat(foundComment.getContent()).isEqualTo("테스트 댓글 내용");
         assertThat(foundComment.isDeleteYn()).isFalse();
         assertThat(foundComment.isActiveYn()).isTrue();
     }
@@ -44,22 +37,32 @@ public class CommentRepositoryTest {
     @Test
     void 기존_댓글을_수정할_수_있어야_한다() {
         //given
+        Comment comment = Comment.builder()
+                .author("테스트 댓글 작성자")
+                .content("테스트 댓글 내용")
+                .deleteYn(false)
+                .activeYn(true)
+                .build();
         Comment savedComment = commentRepository.save(comment);
 
         //when
-        String newCommentContent = "테스트 수정 댓글 내용";
-
-        savedComment.updateCommentContent(newCommentContent);
+        savedComment.updateCommentContent("테스트 수정 댓글 내용");
         Comment foundComment = commentRepository.findById(savedComment.getCommentId()).orElseThrow();
 
         //then
-        assertThat(foundComment.getContent()).isEqualTo(newCommentContent);
+        assertThat(foundComment.getContent()).isEqualTo("테스트 수정 댓글 내용");
     }
 
 
     @Test
     void 기존_댓글을_삭제할_수_있어야_한다() {
         //given
+        Comment comment = Comment.builder()
+                .author("테스트 댓글 작성자")
+                .content("테스트 댓글 내용")
+                .deleteYn(false)
+                .activeYn(true)
+                .build();
         Comment savedComment = commentRepository.save(comment);
 
         //when
